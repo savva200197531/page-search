1c94244eb80a9b635d3b41a5913c4c8615dd0429
savva200197531 <kashin.savva@mail.ru>
2020-09-15 19:20:20 +0700

    transferred checker to separate function

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,26 @@
 const searchBtn = document.querySelector('.search__button');
 const searchWord = document.querySelector('.search__input');
+const allContent = document.querySelector('.table');
 let searchCounter = document.querySelector('.search__counter');
 let counter = 0;
-const allContent = document.querySelector('.table');
 
-searchBtn.addEventListener('click', () => {
+const startSearch = () => {
   const word = searchWord.value;
   if (word.length !== 0) {
     counter = 0;
     highlight(allContent, [ word ]);
   } else {
-    console.log('Поле должно быть заполнено')
     searchCounter.innerHTML = 'Поле должно быть заполнено!';
   }
-})
+}
+
+const checker = (count) => {
+  if (count <= 0) {
+    searchCounter.innerHTML = 'Ничего не найдено.';
+  } else {
+    searchCounter.innerHTML = `Количество совпадений: ${ count }.`;
+  }
+}
 
 const highlight = (elem, keywords, caseSensitive = true, cls = 'highlight') => {
   const flags = caseSensitive ? 'gi' : 'g';
@@ -21,7 +28,7 @@ const highlight = (elem, keywords, caseSensitive = true, cls = 'highlight') => {
   Array.from(elem.childNodes).forEach(child => {
     const keywordRegex = RegExp(keywords.join('|'), flags);
     if (child.nodeType !== 3) {
-      if (counter <= 0) searchCounter.innerHTML = 'Ничего не найдено.'
+      checker(counter);
       highlight(child, keywords, caseSensitive, cls);
     } else if (keywordRegex.test(child.textContent)) {
       const frag = document.createDocumentFragment();
@@ -29,17 +36,13 @@ const highlight = (elem, keywords, caseSensitive = true, cls = 'highlight') => {
       child.textContent.replace(keywordRegex, (match, idx) => {
         const part = document.createTextNode(child.textContent.slice(lastIdx, idx));
         const highlighted = document.createElement('span');
-        counter++
         highlighted.textContent = match;
         highlighted.classList.add(cls);
         frag.appendChild(part);
         frag.appendChild(highlighted);
         lastIdx = idx + match.length;
-        if (counter <= 0) {
-          searchCounter.innerHTML = 'Ничего не найдено.'
-        } else {
-          searchCounter.innerHTML = `Количество совпадений: ${ counter }.`
-        }
+        counter++;
+        checker(counter);
       });
       const end = document.createTextNode(child.textContent.slice(lastIdx));
       frag.appendChild(end);
@@ -47,4 +50,11 @@ const highlight = (elem, keywords, caseSensitive = true, cls = 'highlight') => {
     }
   });
 }
+
+searchBtn.addEventListener('click', startSearch)
+searchWord.addEventListener('keyup', event => {
+  if (event.keyCode === 13) {
+    startSearch();
+  }
+})
 
