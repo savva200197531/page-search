71a6d5812d78c017f003f419b913465d129d2f51
savva200197531 <kashin.savva@mail.ru>
2020-09-15 19:02:38 +0700

    rewrite all stuff + final commit (i think so) + add register sensitive variable

--- app/js/main.js
+++ app/js/main.js
@@ -1,271 +1,50 @@
-// const search = (value, text) => {
-//   const strToFind = new RegExp(text, 'gi')
-//   value.forEach(item => {
-//     if (text) {
-//       item.innerHTML = item.textContent.replace(strToFind, str => `<span class='text-red'>${str}</span>`)
-//     } else {
-//       item.innerHTML = item.textContent.replace(strToFind, str => str)
-//     }
-//     // function highlight(text) {
-//     //   document.body.innerHTML = document.body.innerHTML.replace(
-//     //     new RegExp(text + '(?!([^<]+)?<)', 'gi'),
-//     //     '<b style="background-color:#ff0;font-size:100%">$&</b>'
-//     //   );
-//     // }
-//   })
-// }
-//
-// // const insertMark = (string, pos, length) => string.slice(0, pos) + '<mark>' + string.slice(pos, pos + length) + '</mark>' + string.slice(pos + length)
-//
-//
-// searchWord.addEventListener('keyup', () => {
-//   console.log(searchWord.value)
-//   search(allContent, searchWord.value)
-// })
-
 const searchBtn = document.querySelector('.search__button');
 const searchWord = document.querySelector('.search__input');
-
+let searchCounter = document.querySelector('.search__counter');
+let counter = 0;
 const allContent = document.querySelector('.table');
 
 searchBtn.addEventListener('click', () => {
-  const value = searchWord.value
-  if (value) {
-    InstantSearch.prototype.highlight(allContent, value);
+  const word = searchWord.value;
+  if (word.length !== 0) {
+    counter = 0;
+    highlight(allContent, [ word ]);
   } else {
-    console.log('1')
+    console.log('Поле должно быть заполнено')
+    searchCounter.innerHTML = 'Поле должно быть заполнено!';
   }
 })
 
-class InstantSearch {
-  constructor(props) {
-  }
-
-  highlight(container, highlightText) {
-    console.log(container)
-    const internalHighlighter = options => {
-      const id = {
-          container: "container",
-          tokens: "tokens",
-          all: "all",
-          token: "token",
-          className: "className",
-          sensitiveSearch: "sensitiveSearch"
-        },
-        tokens = options[id.tokens],
-        allClassName = options[id.all][id.className],
-        allSensitiveSearch = options[id.all][id.sensitiveSearch];
-
-
-      const checkAndReplace = (node, tokenArr, classNameAll, sensitiveSearchAll) => {
-        let nodeVal = node.nodeValue, parentNode = node.parentNode,
-          i, j, curToken, myToken, myClassName, mySensitiveSearch,
-          finalClassName, finalSensitiveSearch,
-          foundIndex, begin, matched, end,
-          textNode, span, isFirst;
-
-        for (i = 0, j = tokenArr.length; i < j; i++) {
-          curToken = tokenArr[i];
-          myToken = curToken[id.token];
-          myClassName = curToken[id.className];
-          mySensitiveSearch = curToken[id.sensitiveSearch];
-
-          finalClassName = (classNameAll ? myClassName + " " + classNameAll : myClassName);
-
-          finalSensitiveSearch = (typeof sensitiveSearchAll !== "undefined" ? sensitiveSearchAll : mySensitiveSearch);
-
-          isFirst = true;
-          while (true) {
-            if (finalSensitiveSearch)
-              foundIndex = nodeVal.indexOf(myToken);
-            else
-              foundIndex = nodeVal.toLowerCase().indexOf(myToken.toLowerCase());
-
-            if (foundIndex < 0) {
-              if (isFirst)
-                break;
-
-              if (nodeVal) {
-                textNode = document.createTextNode(nodeVal);
-                parentNode.insertBefore(textNode, node);
-              } // End if (nodeVal)
-              parentNode.removeChild(node);
-              break;
-            } // End if (foundIndex < 0)
-
-            isFirst = false;
-
-
-            begin = nodeVal.substring(0, foundIndex);
-            matched = nodeVal.substr(foundIndex, myToken.length);
-
-            if (begin) {
-              textNode = document.createTextNode(begin);
-              parentNode.insertBefore(textNode, node);
-            } // End if (begin)
-
-            span = document.createElement("span");
-            span.className += finalClassName;
-            span.appendChild(document.createTextNode(matched));
-            parentNode.insertBefore(span, node);
-
-            nodeVal = nodeVal.substring(foundIndex + myToken.length);
-          } // Whend
-
-        } // Next i
-      } // End Function checkAndReplace
-
-      const iterator = (p) => {
-        if (p === null) return;
-
-        let children = Array.prototype.slice.call(p.childNodes), i, cur;
-
-        if (children.length) {
-          for (i = 0; i < children.length; i++) {
-            cur = children[i];
-            if (cur.nodeType === 3) {
-              checkAndReplace(cur, tokens, allClassName, allSensitiveSearch);
-            } else if (cur.nodeType === 1) {
-              iterator(cur);
-            }
-          }
+const highlight = (elem, keywords, caseSensitive = true, cls = 'highlight') => {
+  const flags = caseSensitive ? 'gi' : 'g';
+  keywords.sort((a, b) => b.length - a.length);
+  Array.from(elem.childNodes).forEach(child => {
+    const keywordRegex = RegExp(keywords.join('|'), flags);
+    if (child.nodeType !== 3) {
+      if (counter <= 0) searchCounter.innerHTML = 'Ничего не найдено.'
+      highlight(child, keywords, caseSensitive, cls);
+    } else if (keywordRegex.test(child.textContent)) {
+      const frag = document.createDocumentFragment();
+      let lastIdx = 0;
+      child.textContent.replace(keywordRegex, (match, idx) => {
+        const part = document.createTextNode(child.textContent.slice(lastIdx, idx));
+        const highlighted = document.createElement('span');
+        counter++
+        highlighted.textContent = match;
+        highlighted.classList.add(cls);
+        frag.appendChild(part);
+        frag.appendChild(highlighted);
+        lastIdx = idx + match.length;
+        if (counter <= 0) {
+          searchCounter.innerHTML = 'Ничего не найдено.'
+        } else {
+          searchCounter.innerHTML = `Количество совпадений: ${ counter }.`
         }
-      } // End Function iterator
-
-      iterator(options[id.container]);
+      });
+      const end = document.createTextNode(child.textContent.slice(lastIdx));
+      frag.appendChild(end);
+      child.parentNode.replaceChild(frag, child);
     }
-    internalHighlighter(
-      {
-        container: container
-        , all:
-          {
-            className: "highlighter"
-          }
-        , tokens: [
-          {
-            token: highlightText
-            , className: "highlight"
-            , sensitiveSearch: false
-          }
-        ]
-      }
-    );
-  }
+  });
 }
 
-// const InstantSearch = {
-//
-//   "highlight": function (container, highlightText) {
-//     const internalHighlighter = options => {
-//       const id = {
-//           container: "container",
-//           tokens: "tokens",
-//           all: "all",
-//           token: "token",
-//           className: "className",
-//           sensitiveSearch: "sensitiveSearch"
-//         },
-//         tokens = options[id.tokens],
-//         allClassName = options[id.all][id.className],
-//         allSensitiveSearch = options[id.all][id.sensitiveSearch];
-//
-//
-//       function checkAndReplace(node, tokenArr, classNameAll, sensitiveSearchAll) {
-//         let nodeVal = node.nodeValue, parentNode = node.parentNode,
-//           i, j, curToken, myToken, myClassName, mySensitiveSearch,
-//           finalClassName, finalSensitiveSearch,
-//           foundIndex, begin, matched, end,
-//           textNode, span, isFirst;
-//
-//         for (i = 0, j = tokenArr.length; i < j; i++) {
-//           curToken = tokenArr[i];
-//           myToken = curToken[id.token];
-//           myClassName = curToken[id.className];
-//           mySensitiveSearch = curToken[id.sensitiveSearch];
-//
-//           finalClassName = (classNameAll ? myClassName + " " + classNameAll : myClassName);
-//
-//           finalSensitiveSearch = (typeof sensitiveSearchAll !== "undefined" ? sensitiveSearchAll : mySensitiveSearch);
-//
-//           isFirst = true;
-//           while (true) {
-//             if (finalSensitiveSearch)
-//               foundIndex = nodeVal.indexOf(myToken);
-//             else
-//               foundIndex = nodeVal.toLowerCase().indexOf(myToken.toLowerCase());
-//
-//             if (foundIndex < 0) {
-//               if (isFirst)
-//                 break;
-//
-//               if (nodeVal) {
-//                 textNode = document.createTextNode(nodeVal);
-//                 parentNode.insertBefore(textNode, node);
-//               } // End if (nodeVal)
-//               parentNode.removeChild(node);
-//               break;
-//             } // End if (foundIndex < 0)
-//
-//             isFirst = false;
-//
-//
-//             begin = nodeVal.substring(0, foundIndex);
-//             matched = nodeVal.substr(foundIndex, myToken.length);
-//
-//             if (begin) {
-//               textNode = document.createTextNode(begin);
-//               parentNode.insertBefore(textNode, node);
-//             } // End if (begin)
-//
-//             span = document.createElement("span");
-//             span.className += finalClassName;
-//             span.appendChild(document.createTextNode(matched));
-//             parentNode.insertBefore(span, node);
-//
-//             nodeVal = nodeVal.substring(foundIndex + myToken.length);
-//           } // Whend
-//
-//         } // Next i
-//       } // End Function checkAndReplace
-//
-//       function iterator(p) {
-//         if (p === null) return;
-//
-//         var children = Array.prototype.slice.call(p.childNodes), i, cur;
-//
-//         if (children.length) {
-//           for (i = 0; i < children.length; i++) {
-//             cur = children[i];
-//             if (cur.nodeType === 3) {
-//               checkAndReplace(cur, tokens, allClassName, allSensitiveSearch);
-//             } else if (cur.nodeType === 1) {
-//               iterator(cur);
-//             }
-//           }
-//         }
-//       } // End Function iterator
-//
-//       iterator(options[id.container]);
-//     } // End Function highlighter
-//
-//
-//     internalHighlighter(
-//       {
-//         container: container
-//         , all:
-//           {
-//             className: "highlighter"
-//           }
-//         , tokens: [
-//           {
-//             token: highlightText
-//             , className: "highlight"
-//             , sensitiveSearch: false
-//           }
-//         ]
-//       }
-//     ); // End Call internalHighlighter
-//
-//   } // End Function highlight
-//
-// };
